b6ee285b9e32ab9f9259dd7031eff0cc5185f42e
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-15 17:57:05 +0200

    UI:recipe page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Recipe from "./components/recipe/Recipe";
 
 function App() {
   return (
-    <div className="bg-red-500">
+    <div className="bg-gray-100">
      <Recipe />
     </div>
   );
--- src/components/recipe/Recipe.tsx
+++ src/components/recipe/Recipe.tsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 const Recipe = () => {
   const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("vegetarian");
 
   return (
-    <div>
-      <h1>Food Recipe Hub</h1>
-      <form>
+    <div className="flex items-center flex-col justify-center">
+      <h1 className="text-3xl lg:text-4xl font-bold underline mt-[4rem]">Food Recipe Hub</h1>
+      <form className="flex flex-col lg:flex-row items-center justify-between w-[90%] lg:w-[70%] mt-12">
         <input
           type="text"
           placeholder="Type the Ingredient"
@@ -15,40 +16,47 @@ const Recipe = () => {
           onChange={(e) => {
             setInputValue(e.target.value);
           }}
+          className="h-14 px-8 w-full lg:w-[38%] mb-3 lg:mb-0 rounded-lg shadow-sm outline-none"
         />
-        <select className="app__healthLabels">
+        <select className="h-14 px-8 w-full lg:w-[38%] mb-6 lg:mb-0 rounded-lg shadow-sm outline-none">
           <option
-            value="vegan"
+            value={selectValue}
+            onClick={() => {setSelectValue("vegan");}}
           >
             vegan
           </option>
           <option
-            value="vegetarian"
+            value={selectValue}
+            onClick={() => {setSelectValue("vegetarian");}}
           >
             vegetarian
           </option>
           <option
-            value="low-sugar"
+            value={selectValue}
+            onClick={() => {setSelectValue("low-sugar");}}
           >
             low-sugar
           </option>
           <option
-            value="dairy-free"
+            value={selectValue}
+            onClick={() => {setSelectValue("dairy-free");}}
           >
             dairy-free
           </option>
           <option
-            value=" immuno-supportive "
+            value={selectValue}
+            onClick={() => {setSelectValue("immuno-supportive");}}
           >
             immuno-supportive
           </option>
           <option
-            value="wheat-free"
+            value={selectValue}
+            onClick={() => {setSelectValue("wheat-free");}}
           >
             wheat-free
           </option>
         </select>
-        <button>Get Recipe</button>
+        <button className="p-4 w-[70%] lg:w-[20%] animate-bounce bg-yellow-500 text-white rounded-lg shadow-md">Get Recipe</button>
       </form>
     </div>
   );
