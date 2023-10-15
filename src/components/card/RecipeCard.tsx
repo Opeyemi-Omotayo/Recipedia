e3d5ec0272240b1cec17a72b01fddf55fcd88ed3
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-15 22:50:06 +0200

    feat:ingredients

--- src/components/card/RecipeCard.tsx
+++ src/components/card/RecipeCard.tsx
@@ -1,12 +1,34 @@
 const RecipeCard = ({ recipe }: any) => {
   return (
-    <div className="flex bg-white border border-yellow-500 shadow-lg rounded-lg h-[200px] w-[80%] mb-8">
+    <div
+      className="flex bg-gray-50 hover:bg-white border overflow-hidden border-yellow-500 shadow-lg rounded-lg h-[250px] w-[80%] mb-8"
+      key={Math.random()}
+    >
       <img
         src={recipe["recipe"]["image"]}
-        alt="tile-image"
-        className="w-[250px] mr-4"
+        alt={recipe["recipe"]["label"]}
+        className="w-[250px] "
       />
-      <p className="text-xl font-bold">{recipe["recipe"]["label"]}</p>
+      <div className="w-full p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-xl font-bold">{recipe["recipe"]["label"]}</p>
+          <p className="bg-gray-100 rounded-xl text-gray-600 px-4 py-2">
+            {recipe["recipe"]["dietLabels"][0]}
+          </p>
+        </div>
+
+        <div>
+          <p className="text-base font-semibold pb-2">Ingredients</p>
+          {recipe["recipe"]["ingredientLines"].map(
+            (ingredient: string, index: number) => (
+              <p key={index} className="text-sm">
+                {" "}
+                *{ingredient}
+              </p>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
