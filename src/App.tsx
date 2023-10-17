451c91c3050001d910c6530fe86f355022ed6613
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-17 14:36:15 +0200

    UI:loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import Home from "./pages/Home";
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="bg-gray-100">
       <Home />
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Slide}
+        theme='dark'
+      />
     </div>
   );
 }
--- src/components/recipe/Recipe.tsx
+++ src/components/recipe/Recipe.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import Axios from "axios";
 import RecipeCard from "../card/RecipeCard";
+import { toast } from "react-toastify";
+import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 
 const Recipe = () => {
   const [inputValue, setInputValue] = useState("");
   const [selectValue, setSelectValue] = useState("vegetarian");
   const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const APP_ID = process.env.REACT_APP_EDAMAM_ID;
   const APP_KEY = process.env.REACT_APP_EDAMAM_KEY;
@@ -13,19 +16,29 @@ const Recipe = () => {
   const url = `https://api.edamam.com/search?q=${inputValue}&app_id=${APP_ID}&app_key=${APP_KEY}&health=${selectValue}`;
 
   const getRecipeInfo = async () => {
-    let result = await Axios.get(url);
-    setRecipes(result.data.hits);
+    setIsLoading(true)
+    await Axios.get(url)
+      .then((result) => {
+        setRecipes(result.data.hits);
+        setIsLoading(false);
+      })
+      .catch((error) => toast.error("Couldn't get reciple, please try again"));
   };
 
-  const onSubmit = (e:any) => {
-    e.preventDefault(); 
+  const onSubmit = (e: any) => {
+    e.preventDefault();
     getRecipeInfo();
   };
 
   return (
     <div className="flex items-center flex-col justify-center">
-      <h1 className="text-3xl lg:text-4xl font-bold underline mt-[5rem]">Reci<span className="text-yellow-500">p</span>edia</h1>
-      <form className="flex flex-col lg:flex-row items-center justify-between w-[90%] lg:w-[70%] mt-12 mb-[3rem] lg:mb-[4.5rem]" onSubmit={onSubmit}>
+      <h1 className="text-3xl lg:text-4xl font-bold underline mt-[5rem]">
+        Reci<span className="text-yellow-500">p</span>edia
+      </h1>
+      <form
+        className="flex flex-col lg:flex-row items-center justify-between w-[90%] lg:w-[70%] mt-12 mb-[3rem] lg:mb-[4.5rem]"
+        onSubmit={onSubmit}
+      >
         <input
           type="text"
           placeholder="Type the Ingredient"
@@ -39,51 +52,68 @@ const Recipe = () => {
         <select className="h-14 px-8 w-full lg:w-[38%] mb-6 lg:mb-0 rounded-lg shadow-sm outline-none">
           <option
             value={selectValue}
-            onClick={() => {setSelectValue("vegan");}}
+            onClick={() => {
+              setSelectValue("vegan");
+            }}
           >
             Vegan
           </option>
           <option
             value={selectValue}
-            onClick={() => {setSelectValue("vegetarian");}}
+            onClick={() => {
+              setSelectValue("vegetarian");
+            }}
           >
             Vegetarian
           </option>
           <option
             value={selectValue}
-            onClick={() => {setSelectValue("low-sugar");}}
+            onClick={() => {
+              setSelectValue("low-sugar");
+            }}
           >
             Low-sugar
           </option>
           <option
             value={selectValue}
-            onClick={() => {setSelectValue("dairy-free");}}
+            onClick={() => {
+              setSelectValue("dairy-free");
+            }}
           >
             Dairy-free
           </option>
           <option
             value={selectValue}
-            onClick={() => {setSelectValue("immuno-supportive");}}
+            onClick={() => {
+              setSelectValue("immuno-supportive");
+            }}
           >
             Immuno-supportive
           </option>
           <option
             value={selectValue}
-            onClick={() => {setSelectValue("wheat-free");}}
+            onClick={() => {
+              setSelectValue("wheat-free");
+            }}
           >
             Wheat-free
           </option>
         </select>
-        <button className="p-4 w-[70%] lg:w-[20%] animate-bounce bg-yellow-500 text-white rounded-lg shadow-md">Get Recipe</button>
+        <button className="p-4 w-[70%] lg:w-[20%] animate-bounce bg-yellow-500 text-white rounded-lg shadow-md">
+          Get Recipe
+        </button>
       </form>
+      {isLoading && <LoadingSpinner />}
       {recipes.length === 0 ? (
         <div className="text-center mt-4">
-          <p className="text-red-500 text-lg font-bold h-[70vh]">No recipe available!, Please search again</p>
+          <p className="text-red-500 text-lg font-bold h-[70vh]">
+            No recipe available!, Please search again
+          </p>
         </div>
       ) : (
         recipes.map((recipe, index) => (
-            <RecipeCard recipe={recipe} key={index} />
-          ))
+          <RecipeCard recipe={recipe} key={index} />
+        ))
       )}
     </div>
   );
